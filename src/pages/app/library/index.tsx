c3e0f0ba7f71c0798aa569d843a228576ef15dbd
Guilherme Galaverna <guigalaverna@gmail.com>
2023-06-22 22:04:10 -0300

    feat: add notion integration

--- src/pages/app/library/index.tsx
+++ src/pages/app/library/index.tsx
@@ -1,12 +1,49 @@
 import { Header } from '@/components/Header';
+import { client } from '@/lib/notion';
 import { poppins } from '@/styles/poppins';
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
+import Image from 'next/image';
+import { useState } from 'react';
 
-export default function Library() {
+interface ServerSideProps {
+  books: {
+    results: {
+      id: string;
+      properties: {
+        Título: {
+          title: {
+            plain_text: string;
+          }[];
+        };
+        'Capa do livro': {
+          files: {
+            external: {
+              url: string;
+            };
+          }[];
+        };
+        Categorias: {
+          multi_select: {
+            name: string;
+          }[];
+        };
+        'Está disponível?': {
+          checkbox: boolean;
+        };
+      };
+    }[];
+  };
+}
+
+export default function Library({ books: booksFromNotion }: ServerSideProps) {
+  const [books, setBooks] = useState(booksFromNotion);
+
+  console.log(books);
+
   return (
     <main
-      className={`${poppins.className} w-full h-screen bg-background flex flex-col`}
+      className={`${poppins.className} w-full h-full bg-background flex flex-col`}
     >
       <Header />
 
@@ -17,24 +54,71 @@ export default function Library() {
             Veja os livros que estão disponíveis para reservar e ler
           </p>
 
-          <div className="flex flex-col gap-6 justify-center items-center">
+          <div className="flex gap-2 justify-center items-center my-10">
             <input
               type="text"
               placeholder="O Retrato de Dorian Gray"
-              className="mt-10  w-full p-4 rounded-full bg-zinc-100 border-zinc-200 border-4"
+              className="  w-full p-4 rounded-full bg-zinc-100 border-zinc-200 border-4"
             />
-            <button className="bg-red-500 flex items-center justify-center p-4 text-[1.1rem] w-full rounded-lg text-white gap-6">
+            <button className="bg-red-500 flex items-center justify-center p-4 text-[1.1rem] rounded-lg text-white gap-6">
               Buscar
             </button>
           </div>
         </header>
-        <section></section>
+        <section className="flex flex-wrap items-start gap-6 md:justify-center md:gap-10">
+          {books.results.map(book => {
+            const title = book.properties.Título.title[0].plain_text;
+            const coverUrl =
+              book.properties['Capa do livro'].files[0].external.url;
+            const categories = book.properties.Categorias.multi_select;
+            const isAvailable = book.properties['Está disponível?'].checkbox;
+
+            return (
+              <div key={book.id} className="flex flex-col items-start gap-3">
+                <Image
+                  src={coverUrl}
+                  alt={`Capa do livro ${title}`}
+                  width={200}
+                  height={100}
+                  className="shadow-[7px_7px] shadow-red-500 rounded-lg"
+                />
+
+                <span className="max-w-[200px]">{title}</span>
+                <ul className="flex gap-2 max-w-[200px] flex-wrap">
+                  {categories.map(category => (
+                    <li
+                      key={category.name}
+                      className="bg-red-300 uppercase text-[0.6rem] p-2 text-red-900 font-bold rounded-lg"
+                    >
+                      {category.name}
+                    </li>
+                  ))}
+                </ul>
+                <div className="flex gap-2">
+                  <label htmlFor="isAvailable" className="text-xs">
+                    Está disponível?
+                  </label>
+                  <input
+                    id="isAvailable"
+                    name="isAvailable"
+                    type="checkbox"
+                    checked={isAvailable}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </section>
       </section>
     </main>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
+  const books = await client.databases.query({
+    database_id: process.env.NOTION_DATABASE!,
+  });
+
   const session = getSession(ctx);
 
   if (!session) {
@@ -47,6 +131,8 @@ export const getServerSideProps: GetServerSideProps = async ctx => {
   }
 
   return {
-    props: {},
+    props: {
+      books,
+    },
   };
 };
